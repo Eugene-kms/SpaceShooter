ce2b5aa4c3555098d2449d762e34de162d82e4d6
Eugene-kms <ievgenkolomiiets@gmail.com>
2025-05-03 14:22:21 +0200

    First commit. The App has a problem - not expected byte value.

---
+++ SpaceShooter/MainMenu.swift
@@ -0,0 +1,63 @@
+import SpriteKit
+
+class MainMenu: SKScene {
+        
+        var starField: SKEmitterNode!
+        
+        var newGameButtonNode: SKSpriteNode!
+        var levelGameButtonNode: SKSpriteNode!
+        var labelLevelNode: SKLabelNode!
+        
+        override func didMove(to view: SKView) {
+                
+                starField = self.childNode(withName: "starField_anim") as? SKEmitterNode
+                starField.advanceSimulationTime(10)
+                
+                newGameButtonNode = self.childNode(withName: "newGameButton") as? SKSpriteNode
+                newGameButtonNode.texture = SKTexture(imageNamed: "Complexity")
+                
+                levelGameButtonNode = self.childNode(withName: "levelButton") as? SKSpriteNode
+                levelGameButtonNode.texture = SKTexture(imageNamed: "NewGame")
+                
+                labelLevelNode = self.childNode(withName: "labelLevelButton") as? SKLabelNode
+                
+                let userLevel = UserDefaults.standard
+                
+                if userLevel.bool(forKey: "hard") {
+                        labelLevelNode.text = "Difficult"
+                } else {
+                        labelLevelNode.text = "Easy"
+                }
+                
+        }
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+                let touch = touches.first
+                
+                if let location = touch?.location(in: self) {
+                        let nodesArray = self.nodes(at: location)
+                        
+                        if nodesArray.first?.name == "newGameButton" {
+                                let transition = SKTransition.flipVertical(withDuration: 0.5)
+                                let gameScene = GameScene(size: UIScreen.main.bounds.size)
+                                self.view?.presentScene(gameScene, transition: transition)
+                        } else if nodesArray.first?.name == "levelButton" {
+                                changeLevel()
+                        }
+                }
+        }
+        
+        func changeLevel() {
+                let userLevel = UserDefaults.standard
+                
+                if labelLevelNode.text == "Easy" {
+                        labelLevelNode.text = "Difficult"
+                        userLevel.set(true, forKey: "hard")
+                } else {
+                        labelLevelNode.text = "Easy"
+                        userLevel.set(false, forKey: "hard")
+                }
+                
+                userLevel.synchronize()
+        }
+}
